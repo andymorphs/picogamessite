4c03b1af0b673ad77ac2f44ece1faecf19c8bb9a
andymorphs <100347262+andymorphs@users.noreply.github.com>
2023-08-03 21:02:32 +0100

    update

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,11 +1,11 @@
 // Get Subscribers
     const youtubeKey = 'AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4';
-    const youtubeUser = 'UCt-nBCm1hDI5KVVaXWAR_Ng';
+    const youtubeUser = 'HQ9QcqxgdYXlwtGf6zjnCw';
     const subCount = document.getElementById('subCount');
 
     let getSubscribers = () => {
 
-        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=HQ9QcqxgdYXlwtGf6zjnCw&key=AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4`)
         .then(response => {
             return response.json()
         })

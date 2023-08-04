f1a27078693e3e3718afe06fb8664ac44bda94cf
andrewhoward <imandrewhoward@gmail.com>
2023-08-04 09:42:35 +0100

    update

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,19 +1,13 @@
 // Get Subscribers
-    const APIKey = 'AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4';
-    const channelID = 'HQ9QcqxgdYXlwtGf6zjnCw';
-    const subCount = document.getElementById('subCount');
+const APIKey = "AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4";
+const channelID = "HQ9QcqxgdYXlwtGf6zjnCw";
+const subCount = document.querySelectors(".subCount");
 
-    let getSubscribers = () => {
+const getYouTubeSubs = async () =>{
+    const getData = await axios.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}'
+        );
 
-        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            console.log(data);
-            //subCount.innerHTML = data["items"][0].statistics.subscriberCount;
-        })
+    console.log(getData);
+};
 
-    }
-
-    getSubscribers();
+getYouTubeSubs();

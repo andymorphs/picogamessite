11618fd7d780eade8ae4b0b41199c6a579fbf678
andrewhoward <imandrewhoward@gmail.com>
2023-08-04 09:52:55 +0100

    Update yt.js

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -3,9 +3,11 @@ const APIKey = "AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4";
 const channelID = "HQ9QcqxgdYXlwtGf6zjnCw";
 const subCount = document.querySelector(".subCount");
 
-const getYouTubeSubs = async () =>{
-    const getData = await axios.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}'
-        );
+const getYouTubeSubs = async () => {
+    
+    const getData = await axios.get(
+        'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=HQ9QcqxgdYXlwtGf6zjnCw&key=AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4'
+    );
 
     console.log(getData);
 };

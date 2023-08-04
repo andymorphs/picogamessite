aec9cdcc02c7977071e57a768cc234db212a8075
andrewhoward <imandrewhoward@gmail.com>
2023-08-04 09:46:11 +0100

    Update yt.js

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,7 +1,7 @@
 // Get Subscribers
 const APIKey = "AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4";
 const channelID = "HQ9QcqxgdYXlwtGf6zjnCw";
-const subCount = document.querySelectors(".subCount");
+const subCount = document.querySelector(".subCount");
 
 const getYouTubeSubs = async () =>{
     const getData = await axios.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}'

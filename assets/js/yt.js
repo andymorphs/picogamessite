64708212641004d185e385973a066f9f0f9a4027
andymorphs <100347262+andymorphs@users.noreply.github.com>
2023-08-04 10:30:48 +0100

    Update yt.js

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -3,11 +3,15 @@ const APIKey = "AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4";
 const channelID = "HQ9QcqxgdYXlwtGf6zjnCw";
 //https://www.googleapis.com/youtube/v3/channels?part=statistics&id=andymorphs&key=AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4
 
-fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}`)
-  .then(response => response.json())
-  .then(data => {
-    const subscriberCount = data.items[0].statistics.subscriberCount;
-    // Display the subscriber count on your website
-    document.getElementById('subscriber-count').textContent = `Subscribers: ${subscriberCount}`;
-  })
-  .catch(error => console.error('Error fetching data:', error));
+fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=andymorphs&key=${APIKey}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Log the response to the console for inspection
+        const subscriberCount = data.items[0]?.statistics?.subscriberCount;
+        if (subscriberCount) {
+            document.getElementById('subscriber-count').textContent = `Subscribers: ${subscriberCount}`;
+        } else {
+            document.getElementById('subscriber-count').textContent = `Error: Unable to fetch subscriber count.`;
+        }
+    })
+    .catch(error => console.error('Error fetching data:', error));

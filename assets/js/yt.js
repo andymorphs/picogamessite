8f1abb1108ae28f5069e7d6ca738aa51bf50cad3
andymorphs <100347262+andymorphs@users.noreply.github.com>
2023-08-04 10:27:16 +0100

    Update yt.js

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,6 +1,6 @@
 // Get Subscribers
 const APIKey = "AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4";
-const channelID = "andymorphs";
+const channelID = "HQ9QcqxgdYXlwtGf6zjnCw";
 //https://www.googleapis.com/youtube/v3/channels?part=statistics&id=andymorphs&key=AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4
 
 fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}`)

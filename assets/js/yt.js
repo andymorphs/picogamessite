427f0d3e6e69eeb9fc54a1f35efbac4e943cba8f
andrewhoward <imandrewhoward@gmail.com>
2023-08-04 09:27:02 +0100

    Update yt.js

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,17 +1,17 @@
 // Get Subscribers
-    const youtubeKey = 'AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4';
-    const youtubeUser = 'HQ9QcqxgdYXlwtGf6zjnCw';
+    const APIKey = 'AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4';
+    const channelID = 'HQ9QcqxgdYXlwtGf6zjnCw';
     const subCount = document.getElementById('subCount');
 
     let getSubscribers = () => {
 
-        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=HQ9QcqxgdYXlwtGf6zjnCw&key=AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4`)
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}`)
         .then(response => {
             return response.json()
         })
         .then(data => {
             console.log(data);
-            subCount.innerHTML = data["items"][0].statistics.subscriberCount;
+            //subCount.innerHTML = data["items"][0].statistics.subscriberCount;
         })
 
     }

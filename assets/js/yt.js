76809a14ead5917896a9cd63d6491e04d7176b84
andymorphs <100347262+andymorphs@users.noreply.github.com>
2023-08-04 10:09:45 +0100

    UPDATE
    UPDATE

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,15 +1,13 @@
 // Get Subscribers
 const APIKey = "AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4";
 const channelID = "HQ9QcqxgdYXlwtGf6zjnCw";
-const subCount = document.querySelector(".subCount");
 
-const getYouTubeSubs = async () => {
-    
-    const getData = await axios.get(
-        'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=HQ9QcqxgdYXlwtGf6zjnCw&key=AIzaSyDUPE07TGUM9_9wAV21P1wwJ2VF9hng9y4'
-    );
 
-    console.log(getData);
-};
-
-getYouTubeSubs();
+fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}`)
+  .then(response => response.json())
+  .then(data => {
+    const subscriberCount = data.items[0].statistics.subscriberCount;
+    // Display the subscriber count on your website
+    document.getElementById('subscriber-count').textContent = `Subscribers: ${subscriberCount}`;
+  })
+  .catch(error => console.error('Error fetching data:', error));
